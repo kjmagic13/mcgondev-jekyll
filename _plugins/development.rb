56c92d489a22da76927bb9d1259a96d0b536bb8c
Kevin McCarthy <kjmagic13@gmail.com>
2016-09-06 10:38:21 -0400

    dev plugin variables of collections

--- _plugins/development.rb
+++ _plugins/development.rb
@@ -7,12 +7,14 @@ module Development
 
 	class Theme
 		def initialize(site)
-			@sass_dir = "#{site.in_source_dir}/_sass"
+			@site = site
+			@sass_dir = "#{@site.in_source_dir}/_sass"
 			@theme_file = "#{@sass_dir}/_theme.scss"
 			@theme_dir = "#{@sass_dir}/theme"
 			@files = Dir["#{@theme_dir}/**/*.scss"]
 
 			edit_file
+			# puts list_var_of('properties', 'category')
 		end
 
 		private
@@ -25,6 +27,16 @@ module Development
 				f.write "@import \"theme/#{filename}\";\n" unless filename == '_variables'
 			end
 			f.close
+		end
+
+		def list_var_of(collection, var, unique=true)
+			vars = []
+			dir = @site.collections[collection].directory
+			@site.collections[collection].entries.each do |file|
+				frontmatter_vars = YAML.load File.read("#{dir}/#{file}")
+				vars << frontmatter_vars[var]
+			end
+			unique ? vars.uniq : vars
 		end
 
 	end
